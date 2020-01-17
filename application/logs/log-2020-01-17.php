e5453ee752b95f2605bbcb00e9f5179ade28b5f0
mfajar8 <mfajar16@gmail.com>
2020-01-17 10:51:06 +0700

    First

---
+++ application/logs/log-2020-01-17.php
@@ -0,0 +1,34 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-01-17 04:32:35 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:32:57 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:33:15 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:33:15 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:34:00 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:34:00 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:34:19 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:34:19 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:34:46 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:34:47 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:35:11 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:35:11 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:35:37 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:35:41 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:41:00 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:41:10 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:41:21 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:41:21 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:41:33 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:41:33 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:41:38 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:41:39 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:44:44 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:44:44 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:44:51 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:44:51 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:44:56 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:44:56 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:45:01 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:45:01 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:45:10 --> 404 Page Not Found: /index
+ERROR - 2020-01-17 04:45:11 --> 404 Page Not Found: /index
